7b3fa272e94426046c659bf01b047de90d8db4ac
Amal Sudama <cds.sudama@gmail.com>
2018-05-11 00:24:10 -0400

    wip

--- server/schema/task.js
+++ server/schema/task.js
@@ -24,7 +24,7 @@ const typeDefs = `
     """
     The list of tasks that depends on this task
     """
-    children: [Task]!
+    children: [Task]
   }
 
   type Query {
@@ -37,8 +37,8 @@ const typeDefs = `
     updateTask (
       id: ID!,
       title: String,
-      parents: [ID],
-      children: [ID]
+      parents: [ID!],
+      children: [ID!]
     ): Task
   }
 `
@@ -47,20 +47,17 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    tasks: async () => {
-      const { data } = await axios.get(`${legacyBaseUrl}/tasks`)
-      return data
-    },
-
-    task: async (obj, { id }) => {
-      const { data } = await loaders.task.load(id)
-      return data
-    }
+    tasks: () => axios.get(`${legacyBaseUrl}/tasks`).then(res => res.data),
+    task: (obj, { id }) => loaders.task.load(id).then(res => res.data)
   },
 
   Task: {
-    children: async ({ children }) => await loaders.task.loadMany(children),
-    parents: async ({ parents }) => await loaders.task.loadMany(parents)
+    id: ({ id }) => id,
+    children: ({ id, children }) => {
+      console.log(`[${id}] GET for Children: `, children)
+      return loaders.task.loadMany(children)
+    },
+    parents: ({ parents }) => loaders.task.loadMany(parents)
   },
 
   Mutation: {
